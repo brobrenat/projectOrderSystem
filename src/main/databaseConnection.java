05dc776925227366b9e996ed1d5b743eb31e2f75
Totoben <152851180+Totoben@users.noreply.github.com>
2023-12-07 19:54:14 +0700

    Update databaseConnection.java

--- src/main/databaseConnection.java
+++ src/main/databaseConnection.java
@@ -2,6 +2,7 @@ package main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -52,6 +53,7 @@ public class databaseConnection {
 		}
 		return rs;
 	}
+	
 	public String getLastRegisteredUserID() {
         String lastUserID = null;
         String query = "SELECT userID FROM user ORDER BY userID DESC LIMIT 1";
@@ -67,6 +69,45 @@ public class databaseConnection {
 
         return lastUserID;
     }
+
+	public String getLastRegisteredProdID() {
+		 String lastprodID = null;
+	     String query = "SELECT productID FROM product ORDER BY productID DESC LIMIT 1";
+
+	        try {
+	            ResultSet rs = st.executeQuery(query);
+	            if (rs.next()) {
+	                lastprodID = rs.getString("productID");
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+
+	        return lastprodID;
+	}
+	
+	public boolean deleteProduct(String productName) {
+	    String query = "DELETE FROM product WHERE product_name = ?";
+	    
+	    try {
+	        // Use a prepared statement to avoid SQL injection
+	        try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+	            preparedStatement.setString(1, productName);
+	            
+	            // Execute the update
+	            int rowsAffected = preparedStatement.executeUpdate();
+	            
+	            // Check if the deletion was successful
+	            if (rowsAffected > 0) {
+	                return true;  // Deletion successful
+	            }
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    
+	    return false;  // Deletion failed
+	}
 	
 	public void executeUpdate(String userID,String username, String role, String password, String address, String phoneNum, String gender, String action) {
 		String query = "";
@@ -109,7 +150,25 @@ public class databaseConnection {
 	    return description;
 	}
 
-	
+	public void executeProdAdd(String productID, String product_name, String product_price, String product_des) {
+	    String query = "INSERT INTO product (productID, product_name, product_price, product_des) VALUES (?, ?, ?, ?)";
+	    try {
+	        System.out.println(query);
+
+	        // Use a prepared statement
+	        try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+	            preparedStatement.setString(1, productID);
+	        	preparedStatement.setString(2, product_name);
+	            preparedStatement.setString(3, product_price);
+	            preparedStatement.setString(4, product_des);
+
+	            preparedStatement.executeUpdate();
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+
 	public ResultSet authenticateUser(String username, String password) {
         String query = String.format("SELECT * FROM user WHERE username = \"%s\" AND password = \"%s\"", username, password);
         ResultSet rs = null;
@@ -125,7 +184,7 @@ public class databaseConnection {
 
 	public String getUserRole(String username) {
 	    String query = String.format("SELECT role FROM user WHERE username = '%s'", username);
-	    ResultSet rs = executeQuery(query);
+	    ResultSet rs = executeQuery(username);
 
 	    try {
 	        if (rs != null && rs.next()) {
@@ -156,8 +215,55 @@ public class databaseConnection {
 
         return items;
     }
-	
+    
 
+	public boolean isProductIDExists(String nextprodID) {
+
+		String query = "SELECT COUNT(*) AS count FROM product WHERE productID = ?";
+
+	    try {
+	        // Use a prepared statement to avoid SQL injection
+	        try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+	            preparedStatement.setString(1, nextprodID);
+
+	            // Execute the query
+	            ResultSet resultSet = preparedStatement.executeQuery();
+
+	            // Check the count of rows with the given productID
+	            if (resultSet.next()) {
+	                int count = resultSet.getInt("count");
+	                return count > 0;  // If count > 0, the productID exists
+	            }
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+		return false;
+
+	}
 	
-	
+	public void updateProductPrice(String productName, double newPrice) {
+	    String query = "UPDATE product SET product_price = ? WHERE product_name = ?";
+
+	    try {
+	        // Use a prepared statement to avoid SQL injection
+	        try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+	            preparedStatement.setDouble(1, newPrice);
+	            preparedStatement.setString(2, productName);
+
+	            // Execute the update
+	            int rowsAffected = preparedStatement.executeUpdate();
+
+	            // Check if the update was successful
+	            if (rowsAffected > 0) {
+	                System.out.println("Price updated successfully.");
+	            } else {
+	                System.out.println("Failed to update price. Product not found.");
+	            }
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
+
 }
