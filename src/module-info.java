2f0ff6b29185ec68b8fcbf9e0b9fe127321fd2a6
Totoben <152851180+Totoben@users.noreply.github.com>
2023-12-12 13:47:20 +0700

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,9 @@
 module project {
     requires javafx.controls;
     requires javafx.fxml;
+	requires java.sql;
+	requires javafx.base;
+	requires java.base;
+	requires javafx.graphics;
     opens main;
 }
