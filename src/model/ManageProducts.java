e196b964ba34f6e45edbcd9b55be6f4e9b71a16c
Totoben <152851180+Totoben@users.noreply.github.com>
2023-12-12 13:44:36 +0700

    Update ManageProducts.java

--- src/model/ManageProducts.java
+++ src/model/ManageProducts.java
@@ -78,15 +78,14 @@ public class ManageProducts {
 	GridPane addGP = new GridPane();
 
 	private HBox tombolUpDel = new HBox(upBtn, reBtn);
-	// HBox tombolUpDel = new HBox(upBtn,reBtn);
+	
 	Label eachprice = new Label();
 	VBox intro = new VBox(labelname, labeldesc, eachprice, addBtn, tombolUpDel, addGP);
 	HBox hbox = new HBox(listView, intro);
 	VBox tampilanjudul = new VBox(labeltitle, hbox);
 	Label itemDescriptionTitle = new Label();
 	Label itemDescriptionLabel = new Label();
-	// VBox itemDescriptionBox = new VBox(itemDescriptionTitle,
-	// itemDescriptionLabel, eachprice, addBtn);
+
 	
 
 	public ManageProducts(Stage primaryStage, String username) {
@@ -178,7 +177,7 @@ public class ManageProducts {
 				
 			});
 			
-			//klik tombol update product (upBtn)
+		
 			upBtn.setOnMouseClicked(e -> {
 				labelname.setText(newValue);
 				String productDescription = dbcon.getProductDescription(newValue);
@@ -212,7 +211,7 @@ public class ManageProducts {
 				
 				
 			});
-			// remove button
+			
 			reBtn.setOnMouseClicked(e -> {
 				labelname.setText(newValue);
 				String productDescription = dbcon.getProductDescription(newValue);
@@ -245,27 +244,25 @@ public class ManageProducts {
 			
 			});
 			
-			//saat update product
+		
 			upPro.setOnMouseClicked(e -> {
 			    String selectedProduct = listView.getSelectionModel().getSelectedItem();
 			    String newPriceText = newPrice.getText();
 
-			    // Validasi apakah TextField terisi dan newPrice lebih dari 0
+			    
 			    if (newPriceText.isEmpty() || Double.parseDouble(newPriceText) <= 0) {
-			        // Tampilkan Information Alert jika kondisi tidak memenuhi
+			
 			        showAlert(Alert.AlertType.INFORMATION, "Input tidak valid", "Pastikan harga baru diisi dan lebih dari 0.");
-			        return;  // Hentikan eksekusi jika validasi gagal
+			        return;  
 			    }
 
 			    double newProductPrice = Double.parseDouble(newPriceText);
 
-			    // Update harga pada database
+		
 			    dbcon.updateProductPrice(selectedProduct, newProductPrice);
 
-			    // Perbarui tampilan label harga jika pembaruan berhasil
 			    eachprice.setText("Price: Rp. " + getItemPrice(itemsList, selectedProduct));
 
-			    // Tampilkan Alert sukses
 			    showAlert(Alert.AlertType.INFORMATION, "Pembaruan berhasil", "Harga produk berhasil diperbarui.");
 
 			    labelname.setText(newValue);
@@ -304,13 +301,11 @@ public class ManageProducts {
 			rePro.setOnMouseClicked(e -> {
 			    String selectedProduct = listView.getSelectionModel().getSelectedItem();
 			    
-			    // Validasi: Pastikan ada produk yang dipilih
 			    if (selectedProduct == null || selectedProduct.isEmpty()) {
 			        showAlert(Alert.AlertType.ERROR, "Pilih Produk", "Pilih produk yang akan dihapus.");
 			        return;
 			    }
 
-			    // Hapus produk dari database dan update tampilan
 			    boolean deleteSuccess = deleteProduct(selectedProduct);
 			    if (deleteSuccess) {
 			    	dbcon.deleteProduct(selectedProduct);
@@ -350,7 +345,6 @@ public class ManageProducts {
 			    }
 			});
 			
-			//Klik tomnol back yang add product
 			backBut.setOnMouseClicked(e -> {
 				labelname.setText(newValue);
 				String productDescription = dbcon.getProductDescription(newValue);
@@ -388,46 +382,35 @@ public class ManageProducts {
 				
 			    
 			});
-			// klik tombol add Product untuk add
 			addBut.setOnMouseClicked(e -> {
 				
-				   // Mendapatkan nilai dari input
 			    String newProductName = productName.getText();
 			    String newProductPriceText = productPrice.getText();
 			    String newProductDesc = productDesc.getText();
 			    String newProductID = generateNextProductID();
 
-			    // Validasi: Semua bidang harus diisi
 			    if (newProductName.isEmpty() || newProductPriceText.isEmpty() || newProductDesc.isEmpty()) {
-			        // Menampilkan Error Alert jika salah satu input tidak terisi
 			        showAlert(Alert.AlertType.ERROR, "Input tidak valid", "Semua bidang harus diisi.");
 			        return;
 			    }
 
-			    // Validasi: Nama produk harus unik
 			    if (isProductNameExists(newProductName)) {
-			        // Menampilkan Error Alert jika nama produk sudah ada
 			        showAlert(Alert.AlertType.ERROR, "Input tidak valid", "Nama produk harus unik.");
 			        return;
 			    }
 
-			    // Validasi: Harga produk harus lebih dari 0
 			    double newProductPrice = Double.parseDouble(newProductPriceText);
 			    if (newProductPrice <= 0) {
-			        // Menampilkan Error Alert jika harga produk tidak valid
 			        showAlert(Alert.AlertType.ERROR, "Input tidak valid", "Harga produk harus lebih dari 0.");
 			        return;
 			    }
 
-			    // Jika semua validasi terpenuhi, tambahkan produk ke listview dan database
 			    itemsList.add(new item(newProductName, newProductPrice));
 			    dbcon.executeProdAdd(newProductID, newProductName, newProductPriceText, newProductDesc);
 			    
 
-			    // Update tampilan listview
 			    updateListView();
 
-			    // Tampilkan Information Alert untuk notifikasi berhasil
 			    showAlert(Alert.AlertType.INFORMATION, "Tambah Produk Berhasil", "Produk berhasil ditambahkan.");
 				
 				labelname.setText(newValue);
@@ -538,7 +521,6 @@ public class ManageProducts {
 		
 			});
 		
-		//saat klik button Add Product pertama kali
 		addBtn.setOnMouseClicked(e -> {
 
 			labelname.setText("Welcome, " + username);
@@ -572,7 +554,6 @@ public class ManageProducts {
 			
 		});
 		
-		//Klik tomnol back yang add product
 		backBut.setOnMouseClicked(e -> {
 		    labelname.setText("Welcome, " + username);
 		    labeldesc.setText("Select a product to update");
@@ -584,46 +565,35 @@ public class ManageProducts {
 	        addGP.setVisible(false);;
 		    
 		});
-		// klik tombol add Product untuk add
 		addBut.setOnMouseClicked(e -> {
 			
-			   // Mendapatkan nilai dari input
 		    String newProductName = productName.getText();
 		    String newProductPriceText = productPrice.getText();
 		    String newProductDesc = productDesc.getText();
 		    String newProductID = generateNextProductID();
 
-		    // Validasi: Semua bidang harus diisi
 		    if (newProductName.isEmpty() || newProductPriceText.isEmpty() || newProductDesc.isEmpty()) {
-		        // Menampilkan Error Alert jika salah satu input tidak terisi
 		        showAlert(Alert.AlertType.ERROR, "Input tidak valid", "Semua bidang harus diisi.");
 		        return;
 		    }
 
-		    // Validasi: Nama produk harus unik
 		    if (isProductNameExists(newProductName)) {
-		        // Menampilkan Error Alert jika nama produk sudah ada
 		        showAlert(Alert.AlertType.ERROR, "Input tidak valid", "Nama produk harus unik.");
 		        return;
 		    }
 
-		    // Validasi: Harga produk harus lebih dari 0
 		    double newProductPrice = Double.parseDouble(newProductPriceText);
 		    if (newProductPrice <= 0) {
-		        // Menampilkan Error Alert jika harga produk tidak valid
 		        showAlert(Alert.AlertType.ERROR, "Input tidak valid", "Harga produk harus lebih dari 0.");
 		        return;
 		    }
 
-		    // Jika semua validasi terpenuhi, tambahkan produk ke listview dan database
 		    itemsList.add(new item(newProductName, newProductPrice));
 		    dbcon.executeProdAdd(newProductID, newProductName, newProductPriceText, newProductDesc);
 		    
 
-		    // Update tampilan listview
 		    updateListView();
 
-		    // Tampilkan Information Alert untuk notifikasi berhasil
 		    showAlert(Alert.AlertType.INFORMATION, "Tambah Produk Berhasil", "Produk berhasil ditambahkan.");
 			labelname.setText("Welcome, " + username);
 			labeldesc.setText("Select a product to update");
@@ -648,15 +618,15 @@ public class ManageProducts {
 	private String generateNextProductID() {
 	    String lastProdID = dbcon.getLastRegisteredProdID();
 
-	    String numericPart = lastProdID.substring(2); // Extract the numeric part
+	    String numericPart = lastProdID.substring(2); 
 	    int nextNumber = Integer.parseInt(numericPart) + 1;
 	    
-	    String nextprodID = String.format("TE%04d", nextNumber);
+	    String nextprodID = String.format("TE%03d", nextNumber);
 
-	    // Check if the generated ID already exists, if yes, regenerate
+
 	    while (dbcon.isProductIDExists(nextprodID)) {
 	        nextNumber++;
-	        nextprodID = String.format("TE%04d", nextNumber);
+	        nextprodID = String.format("TE%03d", nextNumber);
 	    }
 
 	    return nextprodID;
@@ -666,20 +636,20 @@ public class ManageProducts {
 		for (item i : itemsList) {
 	        if (i.getObjectname().equals(productName)) {
 	            itemsList.remove(i);
-	            updateListView(); // Move this line outside the loop
-	            return true;  // Hapus berhasil
+	            updateListView(); 
+	            return true;  
 	        }
 	    }
-	    return false;  // Produk tidak ditemukan
+	    return false;  
 	}
 	
 	private boolean isProductNameExists(String productName) {
 	    for (item i : itemsList) {
 	        if (i.getObjectname().equals(productName)) {
-	            return true;  // Nama produk sudah ada
+	            return true;
 	        }
 	    }
-	    return false;  // Nama produk belum ada
+	    return false; 
 	}
 
 	private void updateListView() {
@@ -687,17 +657,17 @@ public class ManageProducts {
 		    for (item i : itemsList) {
 		        items.add(i.getObjectname());
 		    }
-		    listView.setItems(items); // Set the items once outside the loop
+		    listView.setItems(items); 
 	}
 
 	private boolean updateItemPrice(ArrayList<item> items, String itemName, double newPrice) {
 	    for (item i : items) {
 	        if (i.getObjectname().equals(itemName)) {
 	            i.setObjectprice(newPrice);
-	            return true;  // Pembaruan berhasil
+	            return true;  
 	        }
 	    }
-	    return false;  // Pembaruan gagal karena item tidak ditemukan
+	    return false; 
 	}
 	
 	private double getItemPrice(ArrayList<item> items, String itemName) {
@@ -788,10 +758,9 @@ public class ManageProducts {
 	public void loadListData() {
 		
 		if (dbcon != null) {
-	        // Assuming you have a method in your databaseConnection class to retrieve item data
 	        ArrayList<item> itemsFromDatabase = dbcon.getAllItems();
 
-	        itemsList.clear(); // Clear the existing list before adding items from the database
+	        itemsList.clear(); 
 	        itemsList.addAll(itemsFromDatabase);
 
 	        ObservableList<String> items = FXCollections.observableArrayList();
@@ -808,19 +777,19 @@ public class ManageProducts {
 	    listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
 	        
 	    	if (newValue != null) {
-	            // Fetch the product description from the database
+	          
 	            String productDescription = dbcon.getProductDescription(newValue);
 
-	            // Display the product description
+
 	            itemDescriptionLabel.setText(productDescription);
 
-	            // Other code remains the same
+	     
 	            eachprice.setText(String.valueOf(getItemPrice(itemsList, listView.getSelectionModel().getSelectedItem())));
 	            itemDescriptionTitle.setText(newValue);
 	            itemDescriptionLabel.setVisible(true);
 	            double price = getItemPrice(itemsList, newValue);
 	        } else {
-	            // If no product is selected, hide the product description box
+	           
 	            hbox.getChildren().remove(itemDescriptionLabel);
 	            
 	        }
